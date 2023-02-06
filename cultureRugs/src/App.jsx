387f8f5bfdb4323c87af6fdd46df9c4b5c1b936b
poriAgustin <agustindaniele00@gmail.com>
2023-02-06 19:18:31 -0300

    SegundaPreEntrega

--- cultureRugs/src/App.jsx
+++ cultureRugs/src/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Navigate, Route, Routes  } from 'react-router-dom';
+
 import NavBar from './componentes/NavBar/NavBar';
-import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
+import { ItemListContainer } from './componentes/ItemListContainer/ItemListContainer';
+import ItemCount from './componentes/ItemCount/ItemCount';
+import ItemDetailContainer from './componentes/ItemDetailContainer/ItemDetailContainer';
+import CartContainer from './componentes/CartContainer/CartContainer';
 
-function App() {
-  
+// import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  return (
-    <>
+function App(props) { 
+   
+    return (
+        <BrowserRouter>
+                <NavBar />
+                    <div 
+                        // className="border border-5  border-dark mt-1"
+                        // onClick={() => alert('evento de app')}
+                    >
+                        <Routes>
+                            <Route path='/' element = {<ItemListContainer saludo ='Hola'/>}/>
+                            <Route path='/categoria/:idCategoria' element = {<ItemListContainer saludo ='Hola'/>}/>
 
-      <NavBar/>
-      <ItemListContainer saludo='saludo soy pori'/>
+                            <Route path='/detalle/:idProducto' element = {<ItemDetailContainer/>} />
 
-      </>
+                            <Route path='/cart' element = {<CartContainer/>}/>
 
-  )
+                            <Route path='*' element ={ <Navigate to='/' />} />
+                        </Routes>
+                    </div>
+                {/* <ItemCount />  */}
+            </BrowserRouter>
+            
+
+            
+
+       
+    )
 }
 
 export default App
+
--- cultureRugs/src/componentes/NavBar/NavBar.jsx
+++ cultureRugs/src/componentes/NavBar/NavBar.jsx
@@ -1,42 +1,37 @@
+import { Link, NavLink } from 'react-router-dom';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from '../CartWidget/CartWidget';
 
-function CollapsibleExample() {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="black" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">Culture Rugs</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Remeras</Nav.Link>
-            <Nav.Link href="#pricing">Gorras</Nav.Link>
-            <Nav.Link href="#zapatillas">Zapatillas</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
-          </Nav>
-          <Nav>
-            <Nav.Link href="carritodena">🛒</Nav.Link>
-            {/* <Nav.Link eventKey={2} href="#memes">   
-              Dank memes
-            </Nav.Link> */}
-            <cartWidget />
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+const NavBar = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+                <NavLink to='/' >Culture Rugs</NavLink>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <NavLink to="/categoria/gorras" className={({ isActive })=> isActive  ? 'btn btn-primary':'btn btn-outline-primary' } >Gorras</NavLink>
+                    <NavLink to="/categoria/remeras" className={({ isActive })=> isActive  ? 'btn btn-primary':'btn btn-outline-primary' } >Remeras</NavLink>
+                    
+                </Nav>
+                <Nav>
+                    {/* <Nav.Link href="#deets">🛒</Nav.Link> */}
+                    {/* <Nav.Link eventKey={2} href="#memes">
+                    Dank memes
+                    </Nav.Link> */}
+                    <Link to='/cart'>
+                        <CartWidget />
+                    </Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 }
 
-export default CollapsibleExample;
+export default NavBar
